a4e217223e25cde2586ae4352c8e2940ffe28815
caseroomaz <uzeyirhuseynzade008@gmail.com>
2026-03-26 15:07:38 +0400

    Update applecenter.py

--- applecenter.py
+++ applecenter.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "applecenter_2026_premium_key"
 
+# Admin məlumatları
 ADMIN_USER = "admin"
 ADMIN_PASS = "1234"
 
+# -----------------------------------
+# Məhsullar (SƏNİN ORİJİNAL DATA TAM SAXLANIB)
+# -----------------------------------
 products = [
     {
         "id": 1,
@@ -13,8 +17,10 @@ products = [
         "price": 3199,
         "category": "iPhone",
         "images": [
-            "https://store.storeimages.cdn-apple.com/iphone1.jpg",
-            "https://store.storeimages.cdn-apple.com/iphone2.jpg"
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-finish-select-202509-6-9inch_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=NUNzdzNKR0FJbmhKWm5YamRHb05tUzkyK3hWak1ybHhtWDkwUXVINFc0RkZqUFNQc3E5VDh2SEx1ZlJpSjNkR0FOL1haWCt6TDJ0UWlLb09XajVNdENYR1ZZZnEyMVlVQUliTThGMjNyaFFxbm9iakpBWkhjT1hBM3BZeU9zQ0JzNmlxRHcrTG16TVFTaEZGMjZVM3ZB&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-finish-select-202509-6-9inch_AV1?wid=5120&hei=2880&fmt=webp&qlt=90&.v=NUNzdzNKR0FJbmhKWm5YamRHb05tUzkyK3hWak1ybHhtWDkwUXVINFc0RUNxZ2Y2UndFVkhoZG1DQ0NWVTFWa2xjZnhHRHJyenVmME5KTm9Sd1ZaU3NqbWRhTGpRM2xxVWJRWUhSaDlCQ3E0aFZQSlZXTG00RTR2aXlYRzBpVUxlODBad1VqYUZ3RW54YkRKL2hzbXVR&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-finish-select-202509-6-9inch-deepblue?wid=5120&hei=2880&fmt=webp&qlt=90&.v=NUNzdzNKR0FJbmhKWm5YamRHb05tUzkyK3hWak1ybHhtWDkwUXVINFc0RWhhOHJGRUNHdlh6a3VuZVVqdnNrNXVHdDcxbVFRSnhaQ0pnV1pOaG5KaGhNQnJMcnc4RkxJd3ZMc3hKZVVFWHREelVULzVXd2xCbVltNVMyUXhsYlBpMEowc2xaa1ByZlpMdyt3ZFlhVkhn&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-pro-finish-select-202509-6-9inch-silver?wid=5120&hei=2880&fmt=webp&qlt=90&.v=NUNzdzNKR0FJbmhKWm5YamRHb05tUzkyK3hWak1ybHhtWDkwUXVINFc0RVRqUkJqUGFyN1pGMnlaV3JkWU9jdjF1TmpsTkNoRVRMR1N6UXlVZFBaU0NYR1ZZZnEyMVlVQUliTThGMjNyaFFxd1ZHd3R2RmlpWk50MW5LU2N1cWNxdlBsK2ZicnRLY2oza08vTDBZeXZ3&traceId=1"
         ],
         "colors": ["#f5f5f7","#0A1F44","#FF8C00"],
         "likes": 0
@@ -25,9 +31,14 @@ products = [
         "price": 2249,
         "category": "iPhone",
         "images": [
-            "https://www.apple.com/iphone.jpg"
+            "https://www.apple.com/v/iphone-17/e/images/overview/welcome/hero_startframe__e9e7pcnguyqi_xlarge.jpg",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-black_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxa3pYQjBteGp2cFFHL09TNGhVUUhxeHFkSUJZcmNjVXZ4cDk3YTVMcWk4SHF2TWlpSzUzejRCZGt2SjJUNGl1VEE4bm1RcmlWRWp2eDN1WHNkSjNmUlkwQ2hTNHZjREFYdVBRanJ6N1p0WHI&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-white_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxclZmSzgzdlhzQS95ekpRalhXU0JMVkRQR0pzaFhHemZ3ZzZNcDlHRHpJYnF2TWlpSzUzejRCZGt2SjJUNGl1VEE4bm1RcmlWRWp2eDN1WHNkSjNmUmFBdlBzZ01jTzlOOGhYc3dpcENYM2Y&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-lavender_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxbGoraU1aeXdWbEh0SUtyMmtxWGg5dUpDNHBIcmowQ3VoNVJwTm5xckpDV2xjZnhHRHJyenVmME5KTm9Sd1ZaU3NqbWRhTGpRM2xxVWJRWUhSaDlCQ3FTZnZjRTZTT0R6VFJnZ01JbHJqd0hlODBad1VqYUZ3RW54YkRKL2hzbXVR&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-sage_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxZ3VBTlNROXF1MzBwZUoyNEVtMWw3aEtLUmpmVEZhTFpRYkxNWDZBb1R3dGd2S3NaRzcrU0dmYjNHTUFiMnlsWFUxSlgrVWMrMzU1OXo2c2JyNjJZTGcvWXoydVhtUUJyekgyU21tRjFxUUM&traceId=1",
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-mistblue_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxcFYyWWhPSUg0YytZdmJ2dmY4d09xckN0VFdyaFlNakY5MGMxMWhINEhMWmxjZnhHRHJyenVmME5KTm9Sd1ZaU3NqbWRhTGpRM2xxVWJRWUhSaDlCQ3JHYmE3Q0tucGdwdjhDQ1JZbjRxQXRka0xmckVNVTBkS20yTzkwa0dhU09n&traceId=1"
         ],
-        "colors": ["#3a3a3c","#f5f5f7"],
+        "colors": ["#3a3a3c","#f5f5f7","#E6E6FA","#9CAF88","#A9C6D8"],
         "likes": 0
     },
     {
@@ -36,7 +47,7 @@ products = [
         "price": 349,
         "category": "AirPods",
         "images": [
-            "https://store.storeimages.cdn-apple.com/airpods.jpg"
+            "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/airpods-4-anc-select-202409_FV1?wid=976&hei=916&fmt=jpeg&qlt=90"
         ],
         "likes": 0
     }
@@ -44,33 +55,26 @@ products = [
 
 cart_items = []
 
-# ANA SƏHİFƏ
 @app.route("/")
 def home():
     q = request.args.get("q")
-    filtered = [p for p in products if q.lower() in p["name"].lower()] if q else products
+    filtered = [p for p in products if q and q.lower() in p["name"].lower()] if q else products
     return render_template("index.html", products=filtered)
 
-# KATEQORİYA
 @app.route("/category/<cat>")
 def category(cat):
     filtered = [p for p in products if p["category"] == cat]
     return render_template("index.html", products=filtered)
 
-# PRODUCT
 @app.route("/product/<int:id>")
 def product(id):
     p = next((x for x in products if x["id"] == id), products[0])
-
-    # 🔥 ƏLAVƏ (storage yalnız uyğun məhsullar üçün)
     if p["category"] != "AirPods":
         p["storage_list"] = ["128GB","256GB","512GB"]
     else:
         p["storage_list"] = []
-
     return render_template("product.html", product=p)
 
-# CART
 @app.route("/cart")
 def cart():
     total = sum(item['price'] for item in cart_items)
@@ -83,7 +87,6 @@ def cart_add(id):
         cart_items.append(p)
     return redirect("/cart")
 
-# ADMIN LOGIN
 @app.route("/admin/login", methods=["GET","POST"])
 def admin_login():
     if request.method=="POST":
@@ -92,14 +95,12 @@ def admin_login():
             return redirect("/admin")
     return render_template("admin_login.html")
 
-# ADMIN PANEL
 @app.route("/admin")
 def admin():
     if "admin" not in session:
         return redirect("/admin/login")
     return render_template("admin.html", products=products)
 
-# ADD PRODUCT
 @app.route("/admin/add", methods=["POST"])
 def add_product():
     if "admin" not in session:
@@ -116,10 +117,8 @@ def add_product():
         "colors": ["#000","#fff"],
         "likes":0
     })
-
     return redirect("/admin")
 
-# DELETE
 @app.route("/admin/delete/<int:id>")
 def delete_product(id):
     global products
